25b0e66f540ee16b44b0f3c645abae234822bfe0
Anatoly <slabodenyukanatoly@gmail.com>
2023-02-28 23:24:06 +0300

    Add API test

---
+++ src/test/java/ReqresInTests.java
@@ -0,0 +1,22 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class ReqresInTests {
+    @Test
+    void checkChromeVersion() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users?page=2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("page", is(2));
+    }
+}
