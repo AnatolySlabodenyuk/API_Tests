91db11a3c3fa8bfec743247ab5f7cef8102bf3f8
Anatoly Slabodenyuk <slabodenyukanatoly@gmail.com>
2023-03-01 00:31:43 +0300

    Add new tests

--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -1,14 +1,30 @@
-import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasKey;
+import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ReqresInTests {
+
     @Test
-    void checkChromeVersion() {
+    @DisplayName("Checking user first name in Single User")
+    void checkFirstNameInSingleUserPage() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/2")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("data.first_name", is("Janet"));
+    }
+
+
+    @Test
+    @DisplayName("Checking user first name in User List")
+    void checkFirstNameInListUserPage() {
         given()
                 .log().uri()
                 .when()
@@ -17,6 +33,56 @@ public class ReqresInTests {
                 .log().status()
                 .log().body()
                 .statusCode(200)
-                .body("page", is(2));
+                .body("data[0].first_name", is("Michael"));
+    }
+
+    @Test
+    @DisplayName("Checking single user: user not found")
+    void checkSingleUserNotFound() {
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    @DisplayName("Checking authorisation: user not found")
+    void checkErrorAuthorisation() {
+        String data = "{ \"email\": \"somethingEmail@something\", \"password\": \"something\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/login")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("user not found"));
+    }
+
+    @Test
+    @DisplayName("Check successful create user")
+    void createUserTest() {
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
     }
 }
